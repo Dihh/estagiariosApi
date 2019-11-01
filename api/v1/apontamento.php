636db9c321e1d6984c6d5a82372c07b4cb869771
Diegton Rodrigues Barbosa <diegton.rodrigues@dominio.interno>
2019-11-01 18:14:06 -0300

    v1

--- api/v1/apontamento.php
+++ api/v1/apontamento.php
@@ -100,6 +100,3 @@ class apontamentos{
     }
 
 }
-
-//INSERT INTO `apontamentos` (USER, DATA, CHEGADA, ALMOCO, SAIDA) VALUES ('1', '2019-11-01', '09:00:00', '19:00:00', '21:00:00');
-//UPDATE `apontamentos` SET `ALMOCO` = '19:12:00' WHERE `apontamentos`.`ID` = 1;
